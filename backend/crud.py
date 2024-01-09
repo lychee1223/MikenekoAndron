9cdb233c533910351f9e5c5a7956e916f234d4f5
lychee1223 <lychee.vinculum@gmail.com>
2024-01-09 13:38:24 +0900

    [Update]backendのbaselineを作成

--- backend/crud.py
+++ backend/crud.py
@@ -1,9 +1,12 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 
 import models
 import schemas
 from security import hash_password
 
+# ユーザ
+#################################
 
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
@@ -11,9 +14,60 @@ def get_user_by_username(db: Session, username: str):
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
-        username=user.username, hashed_password=hash_password(user.password)
+        username=user.username,
+        hashed_password=hash_password(user.password),
+        is_admin=False
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+# 記事
+#################################
+
+def get_articles(db: Session, skip: int = 0, limit: int = 30):
+    return db.query(models.Article).order_by(desc(models.Article.date)).offset(skip).limit(limit).all()
+
+def get_articles_by_tag(db: Session, tag: str, skip: int = 0, limit: int = 30):
+    return db.query(models.Article).filter(models.Article.tag == tag).order_by(desc(models.Article.date)).offset(skip).limit(limit).all()
+
+def get_article_by_id(db: Session, id: int):
+    return db.query(models.Article).filter(models.Article.id == id).first()
+
+def create_article(db: Session, article: schemas.ArticleCreate):
+    db_article = models.Article(
+        is_works = article.is_works,
+        tag = article.tag,
+        date = article.date,
+        title = article.title,
+        body = article.body,
+    )
+    db.add(db_article)
+    db.commit()
+    db.refresh(db_article)
+    return db_article
+
+# 画像
+#################################
+
+def get_images(db: Session, article_id: int, skip: int = 0, limit: int = 30):
+    return db.query(models.Image).filter(models.Image.article_id == article_id).all()
+
+def get_image_by_path(db: Session, path: str):
+    return db.query(models.Image).filter(models.Image.path == path).first()
+
+def create_image(db: Session, image: schemas.ImageCreate, article_id: int):
+    article = db.query(models.Article).filter(models.Article.id == article_id).first()
+    db_image = models.Image(
+        path = image.path,
+        article_id = article_id,
+    )
+    
+    # 記事に画像を保存
+    article.images.append(db_image)
+
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,18 @@
 from datetime import timedelta
 
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Body, Form, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
+import mimetypes
+from typing import List
+import os
+
 
 import crud
 import schemas
+import markdown
 from db import get_db
 from security import (ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user,
                       create_access_token, get_current_user)
@@ -23,6 +29,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# セキュリティ
+#################################
 
 @app.post("/token", response_model=schemas.Token)
 def login(
@@ -41,15 +49,93 @@ def login(
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+# ユーザ
+#################################
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=user.username)
+    db_user = crud.get_user_by_username(db=db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="同じユーザー名がすでに登録済みです")
     return crud.create_user(db=db, user=user)
 
-
 @app.get("/users/me", response_model=schemas.User)
 def read_user_me(current_user: schemas.User = Depends(get_current_user)):
     return current_user
+
+@app.get("/users/admin", response_model=schemas.User)
+def read_admin_user(current_user: schemas.User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="アクセス権限がありません")
+    return current_user
+
+# 記事
+#################################
+
+@app.post("/articles", response_model=schemas.Article)
+def create_article(article: schemas.ArticleCreate, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="アクセス権限がありません")
+    # Articleを作成し、DBに保存
+    db_article = crud.create_article(db=db, article=article)
+    return db_article
+
+@app.get("/articles", response_model=list[schemas.Article])
+def read_articles(skip: int = 0, limit: int = 30, db: Session = Depends(get_db)):
+    articles = crud.get_articles(db=db, skip=skip, limit=limit)
+
+    # マークダウンをHTMLに変換
+    for article in articles:
+        article.body = markdown.markdown(article.body, extensions=['markdown.extensions.fenced_code'])
+
+    return articles
+
+@app.get("/articles/{tag}", response_model=list[schemas.Article])
+def read_articles_by_tag(tag:str, skip: int = 0, limit: int = 30, db: Session = Depends(get_db)):
+    return crud.get_articles_by_tag(db=db, tag=tag, skip=skip, limit=limit)
+
+# 画像
+#################################
+
+@app.post("/images", response_model=list[schemas.Image])
+def upload_image(article_id: int, images: List[UploadFile] = File(...), db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="アクセス権限がありません")
+
+    # DBにidのデータがあるかチェック
+    # if not crud.get_article_by_id(db=db, id=article_id):
+
+    for image_file in images:
+        image_path = f"public/img/{image_file.filename}"
+
+        # ファイル名が同じ画像が存在する場合はスキップ
+        existing_image = crud.get_image_by_path(db=db, path=image_file.filename)
+        if existing_image:
+            continue
+
+        # 画像をサーバに保存
+        with open(image_path, "wb") as image:
+            image.write(image_file.file.read())
+        
+        # Imageを作成し、DBに保存
+        image_create = schemas.ImageCreate(path=image_file.filename)
+        db_image = crud.create_image(db=db, image=image_create, article_id=article_id)
+    
+    return crud.get_images(db=db, article_id=article_id)
+
+@app.get("/images/{path:path}")
+def read_image(path: str):
+    image_path = f"public/img/{path}"
+
+    # 画像が存在しない場合
+    if not os.path.exists(image_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+    
+    # 画像のメディアタイプを取得
+    media_type, _ = mimetypes.guess_type(image_path)
+
+    # メディアタイプが画像でない場合
+    if not media_type or not media_type.startswith("image/"):
+        raise HTTPException(status_code=415, detail="Unsupported media type")
+
+    return FileResponse(image_path, media_type=media_type)
